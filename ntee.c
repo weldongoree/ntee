7ea8b31b29051379bc6239ac1b6bb32fbe1060dc
Weldon Goree <weldon@b.rontosaur.us>
2016-05-23 13:47:54 +0530

    Working for pipes

--- ntee.c
+++ ntee.c
@@ -3,11 +3,11 @@
 int main(int argc, char **argv) 
 {
 	
-	struct pollfd input_fds[argc - 1];
+	struct pollfd input_fds[argc];
 	int i;
  	char read_buff[64];	
 	input_fds[0].fd = 0; /* stdin */
-	input_fds[0].events = 0;
+	input_fds[0].events = POLLIN | POLLRDNORM | POLLRDBAND | POLLPRI;
 	input_fds[0].revents = 0;
 	ssize_t readcount;
 	for (i = 1; i < argc; i++)
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 			mkfifo(argv[i], 0600);
 		}
 		input_fds[i].fd = open(argv[i], O_RDONLY);
-		input_fds[i].events = POLLRDNORM;
+		input_fds[i].events = POLLIN | POLLRDNORM | POLLRDBAND | POLLPRI;
 		input_fds[i].revents = 0;
 	}
         while (1)
@@ -25,11 +25,13 @@ int main(int argc, char **argv)
 		poll(input_fds, argc, 0);
 		for (i = 0; i < argc; i++)
 		{
-			if (input_fds[i].revents)
+			if (input_fds[i].revents & POLLRDNORM)
 			{
 				readcount=read(input_fds[i].fd, &read_buff, 64);
-		/*		write(1, read_buff, 64); */ 
-				printf("Writing from %d\n", i);
+				if (readcount > 0)
+				{
+					write(1, read_buff, readcount);
+				}
 			}
 		}
 	}				
